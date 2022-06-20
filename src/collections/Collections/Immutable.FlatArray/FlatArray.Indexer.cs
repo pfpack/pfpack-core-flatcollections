07b0fa8e5f79fbda00c2ad987aa82489cd5b4d28
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-06-20 23:06:04 +0300

    Simplify indexer implementation

--- src/collections/Collections/Immutable.FlatArray/FlatArray.Indexer.cs
+++ src/collections/Collections/Immutable.FlatArray/FlatArray.Indexer.cs
@@ -1,18 +1,18 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace System.Collections.Immutable;
+﻿namespace System.Collections.Immutable;
 
 partial class FlatArray<T>
 {
     public T this[int index]
-        =>
-        items[InnerValidateIndex(index)];
+    {
+        get
+        {
+            if ((index >= 0 && index < items.Length) is false)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(index), index, "The index must be greater than or equal to zero and less than the array length.");
+            }
 
-    private int InnerValidateIndex(int index, [CallerArgumentExpression("index")] string? paramName = null)
-        =>
-        index >= 0 && index < items.Length ? index : throw InnerUnexpectedIndexValueException(paramName, index);
-
-    private static ArgumentOutOfRangeException InnerUnexpectedIndexValueException(string? paramName, int index)
-        =>
-        new(paramName, index, "The index must be greater than or equal to zero and less than the array length.");
+            return items[index];
+        }
+    }
 }
