94a30df66c41d175cd6e0114a408a21993e12e21
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-06-20 22:23:26 +0300

    Improve array copy helper

--- src/collections/Collections/Immutable.FlatArray/FlatArray.InnerArrayHelper.cs
+++ src/collections/Collections/Immutable.FlatArray/FlatArray.InnerArrayHelper.cs
@@ -1,20 +1,29 @@
-﻿namespace System.Collections.Immutable;
+﻿//#define array_copy_strategy_static
+//#define array_copy_strategy_instance
+#define array_copy_strategy_clone
+
+namespace System.Collections.Immutable;
 
 partial class FlatArray<T>
 {
     private static class InnerArrayHelper
     {
         internal static T[] Copy(T[] source)
+#if array_copy_strategy_static
         {
-            //var dest = new T[source.Length];
-            //Array.Copy(source, dest, source.Length);
-            //return dest;
-
-            //var dest = new T[source.Length];
-            //source.CopyTo(dest, 0);
-            //return dest;
-
-            return (T[])source.Clone();
+            var dest = new T[source.Length];
+            Array.Copy(source, dest, source.Length);
+            return dest;
         }
+#elif array_copy_strategy_instance
+        {
+            var dest = new T[source.Length];
+            source.CopyTo(dest, 0);
+            return dest;
+        }
+#elif array_copy_strategy_clone
+            =>
+            (T[])source.Clone(); // Currently is faster
+#endif
     }
 }
