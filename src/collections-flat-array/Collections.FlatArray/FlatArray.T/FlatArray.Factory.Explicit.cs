4a1d230c58953fe2f60ba9fed97ddfef8df65d1f
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-06-23 21:41:46 +0300

    Implement the factories

--- src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.Factory.Explicit.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.Factory.Explicit.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 
 namespace System.Collections.Generic;
 
@@ -10,18 +11,90 @@ partial class FlatArray<T>
 
     public static FlatArray<T> From([AllowNull] params T[] source)
         =>
-        source?.Length > 0 ? new(InnerCloneArray(source), default) : InnerEmptyFlatArray.Value;
+        source is not null ? InnerFromArray(source) : InnerEmptyFlatArray.Value;
 
     public static FlatArray<T> From([AllowNull] List<T> source)
         =>
-        InnerFrom(source);
+        source is not null ? InnerFromICollection(source) : InnerEmptyFlatArray.Value;
 
     public static FlatArray<T> From([AllowNull] IEnumerable<T> source)
         =>
-        InnerFrom(source);
+        source switch
+        {
+            null
+            =>
+            InnerEmptyFlatArray.Value,
 
-    private static FlatArray<T> InnerFrom(IEnumerable<T>? source)
+            T[] array
+            =>
+            InnerFromArray(array),
+
+            ImmutableArray<T> immutableArray
+            =>
+            InnerFromImmutableArray(immutableArray),
+
+            ICollection<T> collection
+            =>
+            InnerFromICollection(collection),
+
+            _ =>
+            InnerFromIEnumerable(source)
+        };
+
+    private static FlatArray<T> InnerFromArray(T[] source)
+        =>
+        source.Length > 0 ? new(InnerCloneArray(source), default) : InnerEmptyFlatArray.Value;
+
+    private static FlatArray<T> InnerFromImmutableArray(ImmutableArray<T> source)
+        =>
+        source.IsDefault is false ? InnerFromICollection(source) : InnerEmptyFlatArray.Value;
+
+    private static FlatArray<T> InnerFromICollection(ICollection<T> source)
     {
-        throw new NotImplementedException();
+        var count = source.Count;
+        if (count is not > 0)
+        {
+            return InnerEmptyFlatArray.Value;
+        }
+
+        var array = new T[count];
+        source.CopyTo(array, 0);
+
+        // Clone for the safety purposes
+        return new(InnerCloneArray(array), default);
+    }
+
+    private static FlatArray<T> InnerFromIEnumerable(IEnumerable<T> source)
+    {
+        using var enumerator = source.GetEnumerator();
+
+        if (enumerator.MoveNext() is false)
+        {
+            return InnerEmptyFlatArray.Value;
+        }
+
+        int count = 0;
+        var array = new T[4];
+
+        do
+        {
+            if (count < array.Length)
+            {
+                array[count++] = enumerator.Current;
+            }
+            else
+            {
+                Array.Resize(ref array, array.Length * 2);
+                array[count++] = enumerator.Current;
+            }
+        }
+        while (enumerator.MoveNext());
+
+        if (count < array.Length)
+        {
+            Array.Resize(ref array, count);
+        }
+
+        return new(array, default);
     }
 }
