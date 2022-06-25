a86c8f85ed656fc6f432da954a113fcc298e067b
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-06-25 11:45:52 +0300

    Implement Linq overloads

---
+++ src/collections-flat-array/Collections.FlatArray/FlatArrayExtensions/FlatArrayExtensions.Linq.Overloads.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq;
+
+partial class FlatArrayExtensions
+{
+    // Overload standard Linq extensions to avoid redundant enumeration
+
+    public static int Count<T>(this FlatArray<T> flatArray)
+        =>
+        (flatArray ?? throw new ArgumentNullException(nameof(flatArray)))
+        .Length;
+}
