557d8db0476b79def9c49ea93127f14171f573ec
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-06-25 13:51:47 +0300

    Add Any Linq overload

---
+++ src/collections-flat-array/Collections.FlatArray/FlatArrayExtensions/FlatArrayExtensions.Linq.Any.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq;
+
+partial class FlatArrayExtensions
+{
+    // Overload standard Linq extensions to avoid redundant enumeration
+
+    public static bool Any<T>(this FlatArray<T> source)
+    {
+        _ = source ?? throw new ArgumentNullException(nameof(source));
+
+        return source.Length > 0;
+    }
+}
