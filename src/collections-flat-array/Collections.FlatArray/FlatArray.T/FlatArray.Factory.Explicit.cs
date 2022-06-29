b042e1776e36d8712c9fb51388870e72988a987f
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-06-29 10:53:45 +0300

    Fix InnerFromFlatArray

--- src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.Factory.Explicit.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.Factory.Explicit.cs
@@ -63,7 +63,7 @@ partial class FlatArray<T>
 
     private static FlatArray<T> InnerFromFlatArray(FlatArray<T> source)
         =>
-        source.items.Length > 0 ? new(InnerCloneArray(source), default) : InnerEmptyFlatArray.Value;
+        source.items.Length > 0 ? new(InnerCloneArray(source.items), default) : InnerEmptyFlatArray.Value;
 
     private static FlatArray<T> InnerFromImmutableArray(ImmutableArray<T> source)
     {
