6093efd5971d2f1c0a9cf03ea7e7e27a872748bd
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-06-29 21:39:54 +0300

    Add From ImmutableArray implicits

--- src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.Factory.Implicit.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.Factory.Implicit.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 
 namespace System.Collections.Generic;
 
@@ -13,4 +14,13 @@ partial class FlatArray<T>
     public static implicit operator FlatArray<T>([AllowNull] List<T> source)
         =>
         source is null ? null : From(source);
+
+    public static implicit operator FlatArray<T>(ImmutableArray<T> source)
+        =>
+        From(source);
+
+    [return: NotNullIfNotNull("source")]
+    public static implicit operator FlatArray<T>?(ImmutableArray<T>? source)
+        =>
+        source is null ? null : From(source.Value);
 }
