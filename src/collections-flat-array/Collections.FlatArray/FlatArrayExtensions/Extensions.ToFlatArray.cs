6183ebe7553f4c805ab4242c66e16a03d13c8900
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-06-29 21:34:45 +0300

    Add comments

--- src/collections-flat-array/Collections.FlatArray/FlatArrayExtensions/Extensions.ToFlatArray.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArrayExtensions/Extensions.ToFlatArray.cs
@@ -6,20 +6,31 @@ namespace System.Linq;
 
 partial class FlatArrayExtensions
 {
+    // From Array:
+
     [return: MaybeNull, NotNullIfNotNull("source")]
     public static FlatArray<T> ToFlatArray<T>([AllowNull] this T[] source)
         =>
         source is null ? null : FlatArray<T>.From(source);
+
+
+    // From FlatArray:
 
     [return: MaybeNull, NotNullIfNotNull("source")]
     public static FlatArray<T> ToFlatArray<T>([AllowNull] this FlatArray<T> source)
         =>
         source is null ? null : FlatArray<T>.From(source);
 
+
+    // From List:
+
     [return: MaybeNull, NotNullIfNotNull("source")]
     public static FlatArray<T> ToFlatArray<T>([AllowNull] this List<T> source)
         =>
         source is null ? null : FlatArray<T>.From(source);
+
+
+    // From ImmutableArray:
 
     public static FlatArray<T> ToFlatArray<T>(this ImmutableArray<T> source)
         =>
@@ -30,10 +41,16 @@ partial class FlatArrayExtensions
         =>
         source is null ? null : FlatArray<T>.From(source);
 
+
+    // From IReadOnlyList:
+
     [return: MaybeNull, NotNullIfNotNull("source")]
     public static FlatArray<T> ToFlatArray<T>([AllowNull] this IReadOnlyList<T> source)
         =>
         source is null ? null : FlatArray<T>.From(source);
+
+
+    // From IEnumerable:
 
     [return: MaybeNull, NotNullIfNotNull("source")]
     public static FlatArray<T> ToFlatArray<T>([AllowNull] this IEnumerable<T> source)
