be27510c048f3bcf93d347fe3ad86fa570a5fcbe
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-06-30 08:03:53 +0300

    Add From Span factories

---
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.Factory.Explicit.From.Span.cs
@@ -0,0 +1,37 @@
+﻿namespace System.Collections.Generic;
+
+partial class FlatArray<T>
+{
+    public static FlatArray<T> From(ReadOnlySpan<T> source)
+    {
+        var count = source.Length;
+        if (count is not > 0)
+        {
+            return InnerEmptyFlatArray.Value;
+        }
+
+        var array = new T[count];
+        source.CopyTo(new Span<T>(array));
+
+        // Clone for the safety purposes
+        return new(InnerCloneArray(array), default);
+    }
+
+    public static FlatArray<T> From(Span<T> source)
+    {
+        var count = source.Length;
+        if (count is not > 0)
+        {
+            return InnerEmptyFlatArray.Value;
+        }
+
+        var array = new T[count];
+        source.CopyTo(new Span<T>(array));
+
+        // Clone for the safety purposes
+        return new(InnerCloneArray(array), default);
+    }
+    // An alternative implementation:
+    //=>
+    //From((ReadOnlySpan<T>)source);
+}
