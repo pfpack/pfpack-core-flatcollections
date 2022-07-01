d956e279edaa05a0e5e9a7844bd3cb520910a702
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-07-01 08:21:12 +0300

    Improve indexer range check for performance purposes

--- src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.Indexer.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.Indexer.cs
@@ -4,7 +4,7 @@ partial class FlatArray<T>
 {
     public T this[int index]
         =>
-        index >= 0 && index < items.Length
+        unchecked((uint)index) < (uint)items.Length
         ? items[index]
         : throw new ArgumentOutOfRangeException(
             nameof(index), index, InnerExceptionMessages.IndexRangeReqs);
