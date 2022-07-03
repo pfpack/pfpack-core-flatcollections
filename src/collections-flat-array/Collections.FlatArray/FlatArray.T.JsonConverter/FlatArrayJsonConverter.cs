6f826319aa6528e667999ef1b4ab1a58cacd6a1f
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-07-03 20:53:21 +0300

    FlatArrayJsonConverter: Avoid redundant ItemTypeDefault class

--- src/collections-flat-array/Collections.FlatArray/FlatArray.T.JsonConverter/FlatArrayJsonConverter.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T.JsonConverter/FlatArrayJsonConverter.cs
@@ -6,16 +6,11 @@ namespace System.Collections.Generic;
 
 internal sealed partial class FlatArrayJsonConverter<T> : JsonConverter<FlatArray<T>>
 {
-    private static Type ItemType => ItemTypeDefault.Value;
+    private static Type ItemType => typeof(T);
 
     private readonly JsonConverter<T>? itemConverter;
 
     public FlatArrayJsonConverter([AllowNull] JsonSerializerOptions options)
         =>
         itemConverter = (JsonConverter<T>?)options?.GetConverter(ItemType);
-
-    private static class ItemTypeDefault
-    {
-        internal static readonly Type Value = typeof(T);
-    }
 }
