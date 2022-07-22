1ce195a7496ee8adab5a4b09e4b83b582b827d04
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-07-22 23:09:56 +0300

    Add Debug.Assert to InnerArrayHelper Uncheckeds

--- src/collections-flat-array/Collections.FlatArray/FlatArray.T/InnerArrayHelper/FlatArray.InnerArrayHelper.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T/InnerArrayHelper/FlatArray.InnerArrayHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace System.Collections.Generic;
 
@@ -18,6 +19,8 @@ partial class FlatArray<T>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void ExtendUnchecked(ref T[] array, int newSize)
         {
+            Debug.Assert(newSize > array.Length);
+
             var newArray = new T[newSize];
             Array.Copy(array, newArray, array.Length);
             array = newArray;
@@ -27,6 +30,8 @@ partial class FlatArray<T>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static void TruncateUnchecked(ref T[] array, int newSize)
         {
+            Debug.Assert(newSize >= 0 && newSize < array.Length);
+
             var newArray = new T[newSize];
             Array.Copy(array, newArray, newArray.Length);
             array = newArray;
