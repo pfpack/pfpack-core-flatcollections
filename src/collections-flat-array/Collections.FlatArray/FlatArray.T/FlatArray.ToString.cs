1d20d9e4ed2e21c1d864acdf33669cd1057da944
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-08-04 00:00:42 +0300

    FlatArray: Add ToString

---
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.ToString.cs
@@ -0,0 +1,10 @@
+﻿using static System.FormattableString;
+
+namespace System.Collections.Generic;
+
+partial class FlatArray<T>
+{
+    public override string ToString()
+        =>
+        Invariant($"FlatArray[{typeof(T)}]:{{ \"Length\": {Length} }}");
+}
