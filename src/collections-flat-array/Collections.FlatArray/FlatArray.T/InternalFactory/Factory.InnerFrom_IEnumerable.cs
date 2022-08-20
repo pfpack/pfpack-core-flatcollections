1f09a06b253063b93bb117a30798ebad5f6addb1
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-08-20 13:49:11 +0300

    Add a comment

--- src/collections-flat-array/Collections.FlatArray/FlatArray.T/InternalFactory/Factory.InnerFrom_IEnumerable.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T/InternalFactory/Factory.InnerFrom_IEnumerable.cs
@@ -48,6 +48,7 @@ partial class FlatArray<T>
             return new(array, default);
         }
 
+        // The caller MUST ensure the size and the maxCapacity are GREATER than zero
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static int InnerEstimateCapacity(int size, int maxCapacity)
         {
