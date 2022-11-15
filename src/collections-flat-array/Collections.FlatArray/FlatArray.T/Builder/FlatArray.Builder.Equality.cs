cc280fe7a23ef4728a7ebfb2a908d79c181ec5aa
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-11-16 00:05:24 +0400

    FlatArray Builder: Add Equality

---
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T/Builder/FlatArray.Builder.Equality.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace System.Collections.Generic;
+
+partial struct FlatArray<T>
+{
+    partial struct Builder
+    {
+        public bool Equals(Builder other)
+            =>
+            length == other.Length &&
+            ReferenceEquals(items, other.items) &&
+            isBuilt == other.isBuilt;
+
+        public static bool Equals(Builder left, Builder right)
+            =>
+            left.Equals(right);
+
+        public static bool operator ==(Builder left, Builder right)
+            =>
+            left.Equals(right);
+
+        public static bool operator !=(Builder left, Builder right)
+            =>
+            left.Equals(right) is not true;
+
+        [Obsolete("Equals(object?) on FlatArray<T>.Builder will always throw an exception. Use the Equals(FlatArray<T>.Builder) instead.", error: true)]
+        [DoesNotReturn]
+#pragma warning disable CS0809 // Obsolete member overrides non-obsolete member
+        public override bool Equals([NotNullWhen(true)] object? obj)
+#pragma warning restore CS0809 // Obsolete member overrides non-obsolete member
+            =>
+            throw new NotSupportedException();
+
+        [Obsolete("GetHashCode() on FlatArray<T>.Builder will always throw an exception.", error: true)]
+        [DoesNotReturn]
+#pragma warning disable CS0809 // Obsolete member overrides non-obsolete member
+        public override int GetHashCode()
+#pragma warning restore CS0809 // Obsolete member overrides non-obsolete member
+            =>
+            throw new NotSupportedException();
+    }
+}
