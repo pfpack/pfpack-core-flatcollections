0408c77fc856094423ad39d5c6bdf3e94a6a0400
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-11-19 17:25:46 +0400

    Add static FlatArray.Builder

---
+++ src/collections-flat-array/Collections.FlatArray/FlatArray/Builder/FlatArrayBuilder.From.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace System.Collections.Generic;
+
+partial class FlatArray
+{
+    partial class Builder
+    {
+        public static FlatArray<T>.Builder From<T>([AllowNull] params T[] source)
+            =>
+            FlatArray<T>.Builder.From(source);
+    }
+}
