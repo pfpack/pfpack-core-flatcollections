50c16b7962ab59e3ea56c1d7d18d49cd42c25cef
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-11-20 19:41:50 +0400

    FlatArrayJsonConverterFactory: Minor refactor; Add null-check

--- src/collections-flat-array/Collections.FlatArray/FlatArray.T.JsonConverterFactory/Factory.CreateConverter.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T.JsonConverterFactory/Factory.CreateConverter.cs
@@ -8,13 +8,17 @@ partial class FlatArrayJsonConverterFactory
 {
     public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
     {
+        _ = typeToConvert ?? throw new ArgumentNullException(nameof(typeToConvert));
+
         var itemType = typeToConvert.GetGenericArguments()[0];
 
         return (JsonConverter?)Activator.CreateInstance(
-            type: typeof(FlatArrayJsonConverter<>).MakeGenericType(new[] { itemType }),
+            type: InnerJsonConverterType.MakeGenericType(itemType),
             bindingAttr: BindingFlags.Instance | BindingFlags.Public,
             binder: null,
             args: new object[] { options },
             culture: null);
     }
+
+    private static Type InnerJsonConverterType => typeof(FlatArrayJsonConverter<>);
 }
