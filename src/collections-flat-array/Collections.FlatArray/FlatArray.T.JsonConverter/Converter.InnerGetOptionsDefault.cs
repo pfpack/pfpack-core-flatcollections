936877fd378d708750266cb5495bcb0c5119afc5
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-11-21 22:12:24 +0400

    Add AggressiveInlining to InnerGetOptionsDefault()

--- src/collections-flat-array/Collections.FlatArray/FlatArray.T.JsonConverter/Converter.InnerGetOptionsDefault.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T.JsonConverter/Converter.InnerGetOptionsDefault.cs
@@ -1,9 +1,11 @@
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 
 namespace System.Collections.Generic;
 
 partial class FlatArrayJsonConverter<T>
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static JsonSerializerOptions InnerGetOptionsDefault()
         =>
 #if NET7_0_OR_GREATER
