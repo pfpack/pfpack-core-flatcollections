0f6dc5de872a1badadc2a609207e2cd82a3bb2f0
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-11-23 01:57:14 +0400

    Remove unused exception factories

--- src/collections-flat-array/Collections.FlatArray/FlatList.T/FlatList.InnerExceptionFactory.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatList.T/FlatList.InnerExceptionFactory.cs
@@ -6,10 +6,6 @@ partial class FlatList<T>
 {
     private static class InnerExceptionFactory
     {
-        internal static ArgumentOutOfRangeException IndexOutOfRange(string paramName, int actualValue)
-            =>
-            new(paramName, InnerBuildOutOfRangeMessage("Index must be greater than or equal to zero and less than the array length.", actualValue));
-
         internal static InvalidOperationException EnumerationEitherNotStartedOrFinished()
             =>
             new("Enumeration has either not started or has already finished.");
@@ -17,12 +13,5 @@ partial class FlatList<T>
         internal static NotSupportedException NotSupportedOnReadOnlyCollection()
             =>
             new("The operation is not supported on read-only collection.");
-
-        private static string InnerBuildOutOfRangeMessage<TValue>(string message, TValue actualValue)
-        {
-            var separator = message.TrimEnd().EndsWith('.') ? null : ".";
-
-            return Invariant($"{message}{separator} Actual value was {actualValue}.");
-        }
     }
 }
