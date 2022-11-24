8d8c3a5413583daaf82976045a1eaf1bd4f0dfc0
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-11-24 23:51:16 +0400

    Implement mutable Enumerator for FlatArray Builder

---
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T.Builder/FlatArray.Builder.Enumerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System.Collections.Generic;
+
+partial struct FlatArray<T>
+{
+    partial struct Builder
+    {
+        public ref struct Enumerator
+        {
+            private const int DefaultIndex = -1;
+
+            private readonly Span<T> items;
+
+            private int index;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal Enumerator(Span<T> items)
+            {
+                this.items = items;
+                index = DefaultIndex;
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public bool MoveNext()
+            {
+                int next = index + 1;
+                if (next < items.Length)
+                {
+                    index = next;
+                    return true;
+                }
+
+                return false;
+            }
+
+            public ref T Current
+            {
+                get
+                {
+                    if (index >= 0 && index < items.Length)
+                    {
+                        return ref items[index];
+                    }
+
+                    throw InnerExceptionFactory.EnumerationEitherNotStartedOrFinished();
+                }
+            }
+        }
+    }
+}
