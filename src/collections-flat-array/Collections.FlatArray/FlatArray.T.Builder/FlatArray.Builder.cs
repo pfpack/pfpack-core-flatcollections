25c55b273f143f5ff5b6276bff3629293242619a
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-11-26 16:06:37 +0400

    Add a warning supression

--- src/collections-flat-array/Collections.FlatArray/FlatArray.T.Builder/FlatArray.Builder.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T.Builder/FlatArray.Builder.cs
@@ -2,7 +2,9 @@
 
 partial struct FlatArray<T>
 {
+#pragma warning disable IDE0064 // Make readonly fields writable
     public ref partial struct Builder
+#pragma warning restore IDE0064 // Make readonly fields writable
     {
         private readonly Span<T> span;
 
