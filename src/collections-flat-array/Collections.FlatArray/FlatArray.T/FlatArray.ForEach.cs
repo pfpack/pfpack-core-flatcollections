dbf7cb31b27c160898e29e2b58071df306397b09
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-11-26 18:23:26 +0400

    FlatArray: Add ForEach

---
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.T/FlatArray.ForEach.cs
@@ -0,0 +1,34 @@
+﻿namespace System.Collections.Generic;
+
+partial struct FlatArray<T>
+{
+    public void ForEach(Action<T> action)
+    {
+        _ = action ?? throw new ArgumentNullException(nameof(action));
+
+        if (InnerIsEmpty)
+        {
+            return;
+        }
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            action.Invoke(items[i]);
+        }
+    }
+
+    public void ForEach(Action<int, T> action)
+    {
+        _ = action ?? throw new ArgumentNullException(nameof(action));
+
+        if (InnerIsEmpty)
+        {
+            return;
+        }
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            action.Invoke(i, items[i]);
+        }
+    }
+}
