ce98b2bb9fbf4c0aa3f2384638c9554095758ca2
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-11-26 14:51:25 +0400

    Add factories to FlatArray Builder

--- src/collections-flat-array/Collections.FlatArray/FlatArray.Builder/FlatArray.Builder.Factory.From.cs
+++ src/collections-flat-array/Collections.FlatArray/FlatArray.Builder/FlatArray.Builder.Factory.From.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 
 namespace System.Collections.Generic;
 
@@ -7,6 +8,26 @@ partial class FlatArray
     partial class Builder
     {
         public static FlatArray<T>.Builder From<T>([AllowNull] params T[] source)
+            =>
+            FlatArray<T>.Builder.From(source);
+
+        public static FlatArray<T>.Builder From<T>(FlatArray<T> source)
+            =>
+            FlatArray<T>.Builder.From(source);
+
+        public static FlatArray<T>.Builder From<T>(FlatArray<T>? source)
+            =>
+            FlatArray<T>.Builder.From(source);
+
+        public static FlatArray<T>.Builder From<T>([AllowNull] List<T> source)
+            =>
+            FlatArray<T>.Builder.From(source);
+
+        public static FlatArray<T>.Builder From<T>(ImmutableArray<T> source)
+            =>
+            FlatArray<T>.Builder.From(source);
+
+        public static FlatArray<T>.Builder From<T>(ImmutableArray<T>? source)
             =>
             FlatArray<T>.Builder.From(source);
     }
