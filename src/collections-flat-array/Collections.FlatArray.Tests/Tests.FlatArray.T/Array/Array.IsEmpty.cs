6d3b61b1a64038c2a3057a2e7bbda85992bd88e3
pmosk <pm@ggroupp.ru>
2022-11-28 00:06:40 +0400

    Add tests for FlatArray.IsEmpty and IsNotEmpty

---
+++ src/collections-flat-array/Collections.FlatArray.Tests/Tests.FlatArray.T/Array/Array.IsEmpty.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Collections.FlatArray.Tests;
+
+partial class FlatArrayTest
+{
+    [Fact]
+    public void IsEmpty_SourceIsDefault_ExpectTrue()
+    {
+        var source = default(FlatArray<StructType>);
+        var actual = source.IsEmpty;
+
+        Assert.True(actual);
+    }
+
+    [Theory]
+    [InlineData(EmptyString)]
+    [InlineData(null, SomeString, LowerSomeString, EmptyString, AnotherString)]
+    public void IsEmpty_SourceIsNotDefault_ExpectFalse(params string?[] sourceItems)
+    {
+        var source = TestHelper.CreateFlatArrayByInnerConstructor(sourceItems);
+        var actual = source.IsEmpty;
+
+        Assert.False(actual);
+    }
+}
---
+++ src/collections-flat-array/Collections.FlatArray.Tests/Tests.FlatArray.T/Array/Array.IsNotEmpty.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+
+namespace PrimeFuncPack.Collections.FlatArray.Tests;
+
+partial class FlatArrayTest
+{
+    [Fact]
+    public void IsNotEmpty_SourceIsDefault_ExpectFalse()
+    {
+        var source = default(FlatArray<RefType>);
+        var actual = source.IsNotEmpty;
+
+        Assert.False(actual);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false, true, null)]
+    public void IsNotEmpty_SourceIsNotDefault_ExpectFalse(params bool?[] sourceItems)
+    {
+        var source = TestHelper.CreateFlatArrayByInnerConstructor(sourceItems);
+        var actual = source.IsNotEmpty;
+
+        Assert.True(actual);
+    }
+}
