df76eac74e38686482fc062061f32611ce001b99
pmosk <pm@ggroupp.ru>
2022-11-28 00:02:07 +0400

    Add tests for FlatArray.GetLength

---
+++ src/collections-flat-array/Collections.FlatArray.Tests/Tests.FlatArray.T/Array/Array.Length.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Collections.FlatArray.Tests;
+
+partial class FlatArrayTest
+{
+    [Fact]
+    public void GetLength_SourceIsDefault_ExpectDefault()
+    {
+        var source = default(FlatArray<RefType>);
+
+        var actual = source.Length;
+        const int expected = default;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(Zero)]
+    [InlineData(PlusFifteen, null, MinusOne)]
+    public void GetLength_SourceIsNotDefault_ExpectInnerLength(params int?[] sourceItems)
+    {
+        var source = TestHelper.CreateFlatArrayByInnerConstructor(sourceItems);
+
+        var actual = source.Length;
+        var expected = sourceItems.Length;
+
+        Assert.Equal(expected, actual);
+    }
+}
