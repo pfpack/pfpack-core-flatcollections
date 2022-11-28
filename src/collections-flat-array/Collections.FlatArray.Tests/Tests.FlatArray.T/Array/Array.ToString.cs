db2c8de73bfae8b7a747853d549351aa5e9fe9e7
pmosk <pm@ggroupp.ru>
2022-11-29 01:19:05 +0400

    Add tests FlatArray.ToString

---
+++ src/collections-flat-array/Collections.FlatArray.Tests/Tests.FlatArray.T/Array/Array.ToString.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Collections.FlatArray.Tests;
+
+partial class FlatArrayTest
+{
+    [Fact]
+    public void ToString_SourceIsDefault_ExpectStringContainsZeroAndTypeName()
+    {
+        var source = default(FlatArray<StructType>);
+        var actual = source.ToString();
+
+        Assert.Contains("0", actual, StringComparison.InvariantCulture);
+        Assert.Contains("StructType", actual, StringComparison.InvariantCulture);
+    }
+
+    [Fact]
+    public void ToString_SourceIsNotDefault_ExpectStringContainsLengthAndTypeName()
+    {
+        var sourceItems = new[]
+        {
+            ZeroIdRefType, PlusFifteenIdRefType
+        };
+
+        var source = TestHelper.Initialize(sourceItems);
+        var actual = source.ToString();
+
+        Assert.Contains("2", actual, StringComparison.InvariantCulture);
+        Assert.Contains("RefType", actual, StringComparison.InvariantCulture);
+    }
+}
