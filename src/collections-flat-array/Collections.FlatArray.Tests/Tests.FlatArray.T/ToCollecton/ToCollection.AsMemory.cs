bba30dc6f301a6588d40f8a2e152815e1c806456
pmosk <pm@ggroupp.ru>
2022-11-29 01:46:51 +0400

    Add tests for FlatArray.AsMemory

---
+++ src/collections-flat-array/Collections.FlatArray.Tests/Tests.FlatArray.T/ToCollecton/ToCollection.AsMemory.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Collections.FlatArray.Tests;
+
+partial class FlatArrayTest
+{
+    [Fact]
+    public void AsMemory_SourceIsDefault_ExpectDefault()
+    {
+        var source = default(FlatArray<StructType>);
+
+        var actual = source.AsMemory();
+        var expected = default(ReadOnlyMemory<StructType>);
+
+        Assert.StrictEqual(expected, actual);
+    }
+
+    [Fact]
+    public void AsMemory_SourceIsNotDefault_ExpectSourceItems()
+    {
+        var sourceItems = new[]
+        {
+            PlusFifteenIdRefType, ZeroIdRefType, null, MinusFifteenIdRefType
+        };
+
+        var source = TestHelper.Initialize(sourceItems);
+
+        var actual = source.AsMemory();
+        var expected = sourceItems.AsMemory();
+
+        Assert.StrictEqual(expected, actual);
+    }
+}
