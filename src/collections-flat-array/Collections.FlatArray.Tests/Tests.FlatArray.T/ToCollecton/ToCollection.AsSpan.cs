2d14eb28f1db087c6dcbf61d4802954d2b6792eb
pmosk <pm@ggroupp.ru>
2022-11-29 01:44:46 +0400

    Add tests for FlatArray.AsSpan

---
+++ src/collections-flat-array/Collections.FlatArray.Tests/Tests.FlatArray.T/ToCollecton/ToCollection.AsSpan.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Collections.FlatArray.Tests;
+
+partial class FlatArrayTest
+{
+    [Fact]
+    public void AsSpan_SourceIsDefault_ExpectDefault()
+    {
+        var source = default(FlatArray<RefType?>);
+
+        var actual = source.AsSpan();
+        var expected = default(ReadOnlySpan<RefType?>);
+
+        Assert.True(expected == actual);
+    }
+
+    [Fact]
+    public void AsSpan_SourceIsNotDefault_ExpectSourceItems()
+    {
+        var sourceItems = new[]
+        {
+            SomeTextRecordStruct, AnotherTextRecordStruct
+        };
+
+        var source = TestHelper.Initialize(sourceItems);
+
+        var actual = source.AsSpan();
+        var expected = sourceItems.AsSpan();
+
+        Assert.True(expected == actual);
+    }
+}
