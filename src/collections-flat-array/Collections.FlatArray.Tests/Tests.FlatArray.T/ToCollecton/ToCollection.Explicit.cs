f8cf49e3690ac936ba1b9742f38df62bb3748c95
pmosk <pm@ggroupp.ru>
2022-11-29 00:46:38 +0400

    Add tests for FlatArray.ToCollection

---
+++ src/collections-flat-array/Collections.FlatArray.Tests/Tests.FlatArray.T/ToCollecton/ToCollection.Explicit.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Collections.FlatArray.Tests;
+
+partial class FlatArrayTest
+{
+    [Fact]
+    public void ToArray_SourceIsDefault_ExpectEmptyArray()
+    {
+        var source = default(FlatArray<RecordType>);
+
+        var actual = source.ToArray();
+        var expected = Array.Empty<RecordType>();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void ToArray_SourceIsNotDefault_ExpectInnerItems()
+    {
+        var sourceItems = new RecordStruct?[]
+        {
+            SomeTextRecordStruct, null, default(RecordStruct), AnotherTextRecordStruct
+        };
+
+        var source = TestHelper.Initialize(sourceItems);
+        var actual = source.ToArray();
+
+        Assert.Equal(sourceItems, actual);
+    }
+
+    [Fact]
+    public void ToArray_ThenModifyResult_ExpectInnerStateHasNotChanged()
+    {
+        var sourceItems = new RefType[]
+        {
+            PlusFifteenIdRefType, MinusFifteenIdRefType
+        };
+
+        var source = TestHelper.Initialize(sourceItems);
+        var result = source.ToArray();
+
+        result[0] = ZeroIdRefType;
+        TestHelper.VerifyInnerState(2, sourceItems, source);
+    }
+
+    [Fact]
+    public void ToList_SourceIsDefault_ExpectEmptyList()
+    {
+        var source = default(FlatArray<RecordType>);
+        var actual = source.ToList();
+
+        Assert.Empty(actual);
+    }
+
+    [Fact]
+    public void ToList_SourceIsNotDefault_ExpectInnerItems()
+    {
+        var sourceItems = new RecordType[]
+        {
+            MinusFifteenIdSomeStringNameRecord, ZeroIdNullNameRecord, MinusFifteenIdNullNameRecord
+        };
+
+        var source = TestHelper.Initialize(sourceItems);
+        var actual = source.ToList();
+
+        Assert.Equal(sourceItems, actual);
+    }
+
+    [Fact]
+    public void ToList_ThenModifyResult_ExpectInnerStateHasNotChanged()
+    {
+        var sourceItems = new string[]
+        {
+            SomeString, LowerAnotherString, EmptyString
+        };
+
+        var source = TestHelper.Initialize(sourceItems);
+        var result = source.ToList();
+
+        result[1] = MixedWhiteSpacesString;
+        result.Remove(SomeString);
+
+        TestHelper.VerifyInnerState(3, sourceItems, source);
+    }
+
+    [Fact]
+    public void ToImmutableArray_SourceIsDefault_ExpectEmptyImmutableArray()
+    {
+        var source = default(FlatArray<DateOnly?>);
+
+        var actual = source.ToImmutableArray();
+        var expected = ImmutableArray<DateOnly?>.Empty;
+
+        Assert.StrictEqual(expected, actual);
+    }
+
+    [Fact]
+    public void ToImmutableArray_SourceIsNotDefault_ExpectInnerItems()
+    {
+        var sourceItems = new string[]
+        {
+            "One", "Two", "Three", "Four", "Five"
+        };
+
+        var source = TestHelper.Initialize(sourceItems);
+        var actual = source.ToImmutableArray();
+
+        Assert.Equal(sourceItems, actual);
+    }
+}
