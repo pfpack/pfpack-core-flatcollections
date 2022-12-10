2b3c725dad76d61763edb796dd5eff98d7f5c4ee
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-12-10 23:21:48 +0400

    FlatArray<T>.Builder: Temporarely remove the most basic constructor

--- src/flatcollections-array/FlatArray/FlatArray.T.Builder/FlatArray.Builder.Factory.Constructor.Private.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T.Builder/FlatArray.Builder.Factory.Constructor.Private.cs
@@ -22,20 +22,5 @@ partial struct FlatArray<T>
             length = items.Length;
             this.items = items;
         }
-
-        // Initializes an instance in 'as is' mode without any processing and creation of a defensive copy
-        //
-        // Since the invariant of the Builder implies the empty Builder with zero capacity contains null underlying array,
-        // the caller MUST ensure the length is GREATER than zero
-        /*
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private Builder(int length, T[] items)
-        {
-            Debug.Assert(length > 0 && length <= items.Length);
-
-            this.length = length;
-            this.items = items;
-        }
-        */
     }
 }
