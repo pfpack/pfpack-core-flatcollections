283909ea018da4e76d8cef9e64a202e096132805
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-12-17 09:32:14 +0400

    Revert "Temporarely hide the Builder tests"
    This reverts commit 51a55e408c419a98a81297be1585ef5fea409cc0.

---
+++ src/flatcollections-array/FlatArray.Tests/Tests.FlatArray.T.Builder/Builder/Builder.IsNotEmpty.cs
@@ -0,0 +1,30 @@
+using System;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Core.Tests;
+
+partial class FlatArrayBuilderTest
+{
+    [Fact]
+    public void IsNotEmpty_SourceIsDefault_ExpectTrue()
+    {
+        var source = default(FlatArray<RecordStruct?>.Builder);
+        var actual = source.IsNotEmpty;
+
+        Assert.False(actual);
+    }
+
+    [Theory]
+    [InlineData(One)]
+    [InlineData(MinusFifteen, null, PlusFifteen)]
+    public void IsNotEmpty_SourceIsNotDefault_ExpectFalse(
+        params int?[] sourceItems)
+    {
+        var source = sourceItems.InitializeFlatArrayBuilder();
+        var actual = source.IsNotEmpty;
+
+        Assert.True(actual);
+    }
+}
---
+++ src/flatcollections-array/FlatArray.Tests/Tests.FlatArray.T.Builder/Builder/Builder.ToString.cs
@@ -0,0 +1,36 @@
+using System;
+using PrimeFuncPack.UnitTest;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Core.Tests;
+
+partial class FlatArrayBuilderTest
+{
+    [Fact]
+    public void ToString_SourceIsDefault_ExpectStringContainsZeroAndTypeName()
+    {
+        var source = default(FlatArray<RecordType>.Builder);
+        var actual = source.ToString();
+
+        Assert.Contains("0", actual, StringComparison.InvariantCulture);
+        Assert.Contains("RecordType", actual, StringComparison.InvariantCulture);
+    }
+
+    [Fact]
+    public void ToString_SourceIsNotDefault_ExpectStringContainsLengthAndTypeName()
+    {
+        var sourceItems = new[]
+        {
+            SomeString, EmptyString, null, WhiteSpaceString, AnotherString
+        };
+
+        const int length = 3;
+
+        var source = sourceItems.InitializeFlatArrayBuilder(length);
+        var actual = source.ToString();
+
+        Assert.Contains(length.ToString(), actual, StringComparison.InvariantCulture);
+        Assert.Contains("String", actual, StringComparison.InvariantCulture);
+    }
+}
