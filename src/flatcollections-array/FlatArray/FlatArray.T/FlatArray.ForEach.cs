64b7e18ba735a15cd163d8fbfb950995b45d136b
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-12-26 02:49:16 +0400

    Refactor: avoid using redundant InnerForEach

--- src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.ForEach.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.ForEach.cs
@@ -5,25 +5,19 @@ namespace System;
 partial struct FlatArray<T>
 {
     public void ForEach(Action<T> action)
-        =>
-        InnerForEach(action ?? throw new ArgumentNullException(nameof(action)));
-
-    public void ForEach(Action<int, T> action)
-        =>
-        InnerForEach(action ?? throw new ArgumentNullException(nameof(action)));
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private void InnerForEach(Action<T> action)
     {
+        _ = action ?? throw new ArgumentNullException(nameof(action));
+
         for (int i = 0; i < length; i++)
         {
             action.Invoke(items![i]);
         }
     }
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private void InnerForEach(Action<int, T> action)
+    public void ForEach(Action<int, T> action)
     {
+        _ = action ?? throw new ArgumentNullException(nameof(action));
+
         for (int i = 0; i < length; i++)
         {
             action.Invoke(i, items![i]);
