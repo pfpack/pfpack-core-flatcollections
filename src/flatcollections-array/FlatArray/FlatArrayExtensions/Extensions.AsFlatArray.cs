762527f418ad1e55d9bd877222798a1c7958d12d
Andrei Sergeev <a.sergeev.official@gmail.com>
2022-12-26 01:43:04 +0400

    Add AsFlatArray extension

---
+++ src/flatcollections-array/FlatArray/FlatArrayExtensions/Extensions.AsFlatArray.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System;
+
+partial class FlatArrayExtensions
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static FlatArray<T> AsFlatArray<T>(this T value)
+        =>
+        new(value);
+}
