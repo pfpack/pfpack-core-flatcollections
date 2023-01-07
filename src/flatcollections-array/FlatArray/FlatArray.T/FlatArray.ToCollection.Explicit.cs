4b236dc3c7bc7dd00ccef3641989039fb667be12
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-01-07 14:13:50 +0400

    Improve ToList / ToImmutableArray performance

--- src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.ToCollection.Explicit.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.ToCollection.Explicit.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 namespace System;
@@ -22,16 +23,19 @@ partial struct FlatArray<T>
             return new(items);
         }
 
-        // The most efficient way to build a list for this case:
-
-        return InnerCopyToList(items, length);
+        return InnerToList(items, length);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        static List<T> InnerCopyToList(T[] items, int length)
+        static List<T> InnerToList(T[] items, int length)
         {
-            List<T> result = new(capacity: length);
-            for (int i = 0; i < length; i++) { result.Add(items[i]); }
-            return result;
+            // Passing ArraySegment to the constructor leads to the same efficient behavior
+            // like in the case of 'new(items)' above, i.e., to calling ICollection<T>.CopyTo
+            // and then to calling efficient Array.Copy
+            //
+            // Thus, it should be the most efficient way to build a list in this case
+
+            var effectiveItems = new ArraySegment<T>(items, 0, length);
+            return new(effectiveItems);
         }
     }
 
@@ -47,6 +51,29 @@ partial struct FlatArray<T>
             return ImmutableArray.Create(items);
         }
 
-        return ImmutableArray.Create(items, 0, length);
+        return InnerToImmutable(items, length);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static ImmutableArray<T> InnerToImmutable(T[] items, int length)
+        {
+            //return ImmutableArray.Create(items, 0, length);
+
+            // Simple ImmutableArray.Create(items, start, length) assigns the array items in a cycle
+            // While Builder.AddRange uses Array.Copy like the ImmutableArray.Create(items) above
+            //
+            // Likely, Create(items, start, length) has this behavior for performance purposes
+            // since in general it is intended for copying small array segments
+            //
+            // But we need to copy the whole array within its effective length
+            // Thus, using the Builder might be more efficient in this case
+
+            var builder = ImmutableArray.CreateBuilder<T>(initialCapacity: length);
+            builder.AddRange(items, length);
+
+            // Call MoveToImmutable instead of ToImmutable to avoid creating redundant defensive copy
+            // We've ensured Builder.Count equals Builder.Capacity to avoid throwing the exception
+            Debug.Assert(builder.Count == builder.Capacity);
+            return builder.MoveToImmutable();
+        }
     }
 }
