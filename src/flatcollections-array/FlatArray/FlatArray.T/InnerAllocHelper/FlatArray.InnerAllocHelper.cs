9a84a71c96829581082d9af77e719ff4b08d3a08
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-01-08 17:50:55 +0400

    Refactor InnerAllocHelper

--- src/flatcollections-array/FlatArray/FlatArray.T/InnerAllocHelper/FlatArray.InnerAllocHelper.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/InnerAllocHelper/FlatArray.InnerAllocHelper.cs
@@ -10,49 +10,39 @@ partial struct FlatArray<T>
         // Default capacity for cases where capacity must be greater than zero
         internal const int DefaultPositiveCapacity = 4;
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static int EnsurePositiveCapacity(int capacity)
             =>
             capacity > 0 ? capacity : DefaultPositiveCapacity;
 
-        // The caller MUST ensure the length is GREATER than zero
-        // and the max capacity is NOT LESS than the length
+        // The caller MUST ensure the length is GREATER than zero,
+        // and the length is LESS than or EQUAL to the max capacity
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static int EstimateCapacity(int length, int maxCapacity)
         {
             Debug.Assert(length > 0);
-            Debug.Assert(maxCapacity >= length);
+            Debug.Assert(length <= maxCapacity);
 
-            int capacity = unchecked(length * 2);
+            int doubleLength = unchecked(length * 2);
 
-            if (capacity < 0) // handle the overflow case
-            {
-                return maxCapacity;
-            }
-
-            if (capacity > maxCapacity)
-            {
-                return maxCapacity;
-            }
-
-            return capacity;
+            return unchecked((uint)doubleLength) <= unchecked((uint)maxCapacity)
+                ? doubleLength
+                : maxCapacity;
         }
 
-        // The caller MUST ensure the length is GREATER than zero
-        // and the capacity is NOT LESS than the length
+        // The caller MUST ensure the length is GREATER than zero,
+        // and the length is LESS than or EQUAL to the capacity
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static bool IsHugeCapacity(int length, int capacity)
         {
             Debug.Assert(length > 0);
-            Debug.Assert(capacity >= length);
+            Debug.Assert(length <= capacity);
+
+            // Double length within the capacity means a huge capacity
 
             int doubleLength = unchecked(length * 2);
 
-            if (doubleLength < 0) // handle the overflow case
-            {
-                return false;
-            }
-
-            return doubleLength <= capacity;
+            return unchecked((uint)doubleLength) <= unchecked((uint)capacity);
         }
     }
 }
