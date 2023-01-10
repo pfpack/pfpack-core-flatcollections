069b1f9ea6fead8c7fb66a1006effb7912712375
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-01-10 11:11:17 +0400

    Add a comment

--- src/flatcollections-array/FlatArray/FlatArray.T/InnerBufferHelper/FlatArray.InnerBufferHelper.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/InnerBufferHelper/FlatArray.InnerBufferHelper.cs
@@ -12,6 +12,7 @@ partial struct FlatArray<T>
             int newCapacity = array.Length < Array.MaxLength
                 ? InnerAllocHelper.IncreaseCapacity(array.Length, Array.MaxLength)
                 : throw InnerExceptionFactory.SourceTooLarge();
+                //: array.Length + 1; // Delegate throwing an exception to CLR
 
             InnerArrayHelper.ExtendUnchecked(ref array, newCapacity);
         }
