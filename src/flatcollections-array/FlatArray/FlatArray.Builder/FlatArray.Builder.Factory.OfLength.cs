162fc6d5eeeeb4e79d59b505dd222c82a3ddc5e1
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-01-11 20:00:30 +0400

    Builder: Open static OfLength factory; Add the tests

--- src/flatcollections-array/FlatArray/FlatArray.Builder/FlatArray.Builder.Factory.OfLength.cs
+++ src/flatcollections-array/FlatArray/FlatArray.Builder/FlatArray.Builder.Factory.OfLength.cs
@@ -4,8 +4,7 @@ partial class FlatArray
 {
     partial class Builder
     {
-        // TODO: Make public when the test is implemented
-        internal static FlatArray<T>.Builder OfLength<T>(int length)
+        public static FlatArray<T>.Builder OfLength<T>(int length)
             =>
             FlatArray<T>.Builder.InternalOfLength(length, nameof(length));
     }
