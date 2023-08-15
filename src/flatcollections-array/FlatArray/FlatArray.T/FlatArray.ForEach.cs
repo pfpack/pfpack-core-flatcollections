fc17959a597fdfa476be14b4278732b958ccc236
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-08-15 22:26:42 +0400

    Revert "Restore for cycle in JsonConverter Write"
    This reverts commit cd2f9dc0e27021dcd04d535857b1781ef4022ff7.

--- src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.ForEach.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.ForEach.cs
@@ -6,20 +6,20 @@ partial struct FlatArray<T>
 {
     public void ForEach(Action<T> action)
         =>
-        InnerForEach(action ?? throw new ArgumentNullException(nameof(action)));
+        InternalForEach(action ?? throw new ArgumentNullException(nameof(action)));
 
     public void ForEach(Action<int, T> action)
         =>
-        InnerForEach(action ?? throw new ArgumentNullException(nameof(action)));
+        InternalForEach(action ?? throw new ArgumentNullException(nameof(action)));
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private void InnerForEach(Action<T> action)
+    internal void InternalForEach(Action<T> action)
     {
         for (int i = 0; i < length; i++) { action.Invoke(items![i]); }
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private void InnerForEach(Action<int, T> action)
+    internal void InternalForEach(Action<int, T> action)
     {
         for (int i = 0; i < length; i++) { action.Invoke(i, items![i]); }
     }
