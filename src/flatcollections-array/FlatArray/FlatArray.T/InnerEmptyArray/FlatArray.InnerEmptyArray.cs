be49d920733055a02abaa1ab01c2e04323a1ff67
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-08-16 21:22:41 +0400

    Fix a comment

--- src/flatcollections-array/FlatArray/FlatArray.T/InnerEmptyArray/FlatArray.InnerEmptyArray.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/InnerEmptyArray/FlatArray.InnerEmptyArray.cs
@@ -5,7 +5,7 @@ partial struct FlatArray<T>
     private static class InnerEmptyArray
     {
         // Intended for the inner use only, not to pass outside
-        // Designed to not expose the inner state of the empty FlatArray and the Builder
+        // Designed not to expose the inner state of the empty FlatArray and the Builder
 
 #pragma warning disable CA1825 // Avoid zero-length array allocations
         internal static readonly T[] Value = new T[0];
