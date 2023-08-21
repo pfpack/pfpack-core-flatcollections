1decb8a87163e4d6fffd3e158c25cc9ce39590fb
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-08-22 00:55:45 +0400

    Minor refactor

--- src/flatcollections-array/FlatArray/FlatArray.T.Builder/FlatArray.Builder.Capacity.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T.Builder/FlatArray.Builder.Capacity.cs
@@ -11,7 +11,7 @@ partial struct FlatArray<T>
 
             set
             {
-                if (value >= length is not true)
+                if (InnerAllocHelper.IsWithin(length, value) is not true)
                 {
                     throw InnerBuilderExceptionFactory.CapacityOutOfRange_MustBeGreaterThanOrEqualToLength(value, length);
                 }
