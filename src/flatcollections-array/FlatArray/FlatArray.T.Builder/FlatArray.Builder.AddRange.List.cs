9474fda86c784d8016883735e396bde41232f075
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-08-24 00:13:24 +0400

    Add Builder.AddRange.List

---
+++ src/flatcollections-array/FlatArray/FlatArray.T.Builder/FlatArray.Builder.AddRange.List.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace System;
+
+partial struct FlatArray<T>
+{
+    partial class Builder
+    {
+        // TODO: Add the tests and make public
+        internal Builder AddRange([AllowNull] List<T> items)
+        {
+            if (items is null || items.Count == default)
+            {
+                return this;
+            }
+
+            InnerAddRange(items, items.Count);
+            return this;
+        }
+
+        // TODO: Add the tests and make public
+        internal Builder AddRange([AllowNull] List<T> items, int length)
+        {
+            var itemsLength = items?.Count ?? default;
+
+            if (InnerAllocHelper.IsWithinLength(length, itemsLength) is not true)
+            {
+                throw InnerExceptionFactory.StartSegmentIsNotWithinArray(nameof(length), length, itemsLength);
+            }
+
+            if (items is null || items.Count == default)
+            {
+                return this;
+            }
+
+            InnerAddRange(items, length);
+            return this;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void InnerAddRange(List<T> items, int length)
+        {
+            Debug.Assert(items.Count != default);
+            Debug.Assert(length > 0 && length <= items.Count);
+
+            InnerBufferHelperEx.EnsureBufferCapacity(ref this.items, this.length, length);
+            items.CopyTo(0, this.items, this.length, length);
+            this.length += length;
+        }
+    }
+}
