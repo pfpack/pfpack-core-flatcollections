dec2e9fe11a7e95f7a8d19a6dc8f6f03e616bc44
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-08-24 09:59:21 +0400

    Add Builder AddRange from ReadOnlySpan

---
+++ src/flatcollections-array/FlatArray/FlatArray.T.Builder/FlatArray.Builder.AddRange.ReadOnlySpan.cs
@@ -0,0 +1,22 @@
+﻿namespace System;
+
+partial struct FlatArray<T>
+{
+    partial class Builder
+    {
+        // TODO: Add the tests and make public
+        internal Builder AddRange(ReadOnlySpan<T> items)
+        {
+            if (items.IsEmpty)
+            {
+                return this;
+            }
+
+            InnerBufferHelperEx.EnsureBufferCapacity(ref this.items, length, items.Length);
+            items.CopyTo(new Span<T>(this.items, length, items.Length));
+            length += items.Length;
+
+            return this;
+        }
+    }
+}
