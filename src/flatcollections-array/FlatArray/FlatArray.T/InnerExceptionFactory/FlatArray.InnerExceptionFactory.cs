8ebbe20f4ba31d93ce457c0c0d1f705d867437a0
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-08-30 10:24:26 +0400

    Remove redundant Debug Asserts

--- src/flatcollections-array/FlatArray/FlatArray.T/InnerExceptionFactory/FlatArray.InnerExceptionFactory.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/InnerExceptionFactory/FlatArray.InnerExceptionFactory.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using static System.FormattableString;
+﻿using static System.FormattableString;
 
 namespace System;
 
@@ -8,27 +7,16 @@ partial struct FlatArray<T>
     private static class InnerExceptionFactory
     {
         internal static ArgumentOutOfRangeException StartSegmentOutsideBounds(string paramName, int segmentLength, int arrayLength)
-        {
-            Debug.Assert(segmentLength >= 0);
-            Debug.Assert(arrayLength >= 0);
-
-            return new(paramName, Invariant($"Start segment must be within the array bounds. Start segment length was {segmentLength}. Array length was {arrayLength}."));
-        }
+            =>
+            new(paramName, Invariant($"Start segment must be within the array bounds. Start segment length was {segmentLength}. Array length was {arrayLength}."));
 
         internal static ArgumentOutOfRangeException SegmentOutsideBounds(int segmentStart, int segmentLength, int arrayLength)
-        {
-            Debug.Assert(segmentLength >= 0);
-            Debug.Assert(arrayLength >= 0);
-
-            return new(null, Invariant($"Segment must be within the array bounds. Segment start was {segmentStart}. Segment length was {segmentLength}. Array length was {arrayLength}."));
-        }
+            =>
+            new(null, Invariant($"Segment must be within the array bounds. Segment start was {segmentStart}. Segment length was {segmentLength}. Array length was {arrayLength}."));
 
         internal static IndexOutOfRangeException IndexOutOfRange(int index, int length)
-        {
-            Debug.Assert(length >= 0);
-
-            return new(Invariant($"Index must be greater than or equal to zero and less than the array length. Index was {index}. Array length was {length}."));
-        }
+            =>
+            new(Invariant($"Index must be greater than or equal to zero and less than the array length. Index was {index}. Array length was {length}."));
 
         internal static InvalidOperationException EnumerationEitherNotStartedOrFinished()
             =>
