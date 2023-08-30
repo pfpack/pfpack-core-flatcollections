db8b0f36cb92366a1138486e47b924bc4f4cf608
Andrei Sergeev <a.sergeev.official@gmail.com>
2023-08-30 09:55:09 +0400

    Improve FlatList CopyTo

--- src/flatcollections-array/FlatArray/FlatArray.T.FlatList/FlatArray.FlatList.Impl.Inner.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T.FlatList/FlatArray.FlatList.Impl.Inner.cs
@@ -24,9 +24,45 @@ partial struct FlatArray<T>
             Array.IndexOf(items, item, 0, length);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void InnerCopyToChecked(T[] array, int arrayIndex)
-            =>
-            Array.Copy(items, 0, array, arrayIndex, length); // Delegate null and range checks to Array.Copy
+        private void InnerCopyToChecked(
+            T[] array,
+            int arrayIndex,
+            [CallerArgumentExpression(nameof(array))] string arrayParamName = "",
+            [CallerArgumentExpression(nameof(arrayIndex))] string arrayIndexParamName = "")
+        {
+            _ = array ?? throw new ArgumentNullException(arrayParamName);
+
+            InnerValidateRange();
+
+            if (length == default)
+            {
+                return;
+            }
+
+            var sourceSpan = length == items.Length
+                ? new ReadOnlySpan<T>(items)
+                : new ReadOnlySpan<T>(items, 0, length);
+
+            var destSpan = new Span<T>(array, arrayIndex, array.Length - arrayIndex);
+
+            sourceSpan.CopyTo(destSpan);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            void InnerValidateRange()
+            {
+                if (arrayIndex is not >= 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        arrayIndexParamName, arrayIndex, "Array index must be greater than or equal to zero.");
+                }
+
+                if (InnerAllocHelper.IsSegmentWithinLength(arrayIndex, length, array.Length) is not true)
+                {
+                    throw new ArgumentException(
+                        "The number of elements in the source array must be less than or equal to the available space from the array index to the end of the destination array.");
+                }
+            }
+        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private IEnumerator<T> InnerGetEnumerator()
