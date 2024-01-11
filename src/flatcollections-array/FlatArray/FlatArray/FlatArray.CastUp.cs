4286b4119d66500bbf2bfe52b1e8bac5c77f2405
pmosk <pm@ggroupp.ru>
2024-01-11 14:00:12 +0400

    Add tests for FlatArray.CastUp

--- src/flatcollections-array/FlatArray/FlatArray/FlatArray.CastUp.cs
+++ src/flatcollections-array/FlatArray/FlatArray/FlatArray.CastUp.cs
@@ -2,8 +2,7 @@
 
 partial class FlatArray
 {
-    // TODO: Add the tests and make public
-    internal static FlatArray<T> CastUp<T, TDerived>(FlatArray<TDerived> items)
+    public static FlatArray<T> CastUp<T, TDerived>(FlatArray<TDerived> items)
         where TDerived : class?, T
         =>
         FlatArray<T>.CastUp(items);
