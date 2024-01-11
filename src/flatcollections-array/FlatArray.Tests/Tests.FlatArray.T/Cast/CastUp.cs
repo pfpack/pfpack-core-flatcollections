11bc00d82be8006c214079514e8d6a3d00f82da6
pmosk <pm@ggroupp.ru>
2024-01-11 13:10:36 +0400

    Impl tests for CastUp

---
+++ src/flatcollections-array/FlatArray.Tests/Tests.FlatArray.T/Cast/CastUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Core.Tests;
+
+partial class FlatArrayTest
+{
+    [Theory]
+    [MemberData(nameof(CastUp_ExpectCorrectInnerState_TestSource))]
+    public static void CastUp_ExpectCorrectInnerState(
+        FlatArray<string?> source, object?[]? expectedItems)
+    {
+        var actual = FlatArray<object?>.CastUp(source);
+        actual.VerifyTruncatedState(expectedItems);
+    }
+
+    public static TheoryData<FlatArray<string?>, object?[]?> CastUp_ExpectCorrectInnerState_TestSource
+        =>
+        new()
+        {
+            {
+                default,
+                default
+            },
+            {
+                new string?[] { SomeString, EmptyString }.InitializeFlatArray(),
+                [ SomeString, EmptyString ]
+            },
+            {
+                new string?[] { AnotherString, UpperSomeString, null, WhiteSpaceString, SomeString, TabString }.InitializeFlatArray(5),
+                [ AnotherString, UpperSomeString, null, WhiteSpaceString, SomeString]
+            }
+        };
+}
