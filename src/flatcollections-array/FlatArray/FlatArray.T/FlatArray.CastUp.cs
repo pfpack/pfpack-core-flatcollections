ca5aa88841336efc25d25de72bdfda235556d69b
Andrei Sergeev <a.sergeev.official@gmail.com>
2024-01-11 09:01:32 +0400

    Format code

--- src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.CastUp.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.CastUp.cs
@@ -3,7 +3,8 @@
 partial struct FlatArray<T>
 {
     // TODO: Add the tests and make public
-    internal static FlatArray<T> CastUp<TDerived>(FlatArray<TDerived> items) where TDerived : class?, T
+    internal static FlatArray<T> CastUp<TDerived>(FlatArray<TDerived> items)
+        where TDerived : class?, T
         =>
         new(items.length, items.items, default);
 }
