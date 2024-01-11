857e81518f271649f7ced13a46561eedf3ec9b5e
Andrei Sergeev <a.sergeev.official@gmail.com>
2024-01-11 10:13:44 +0400

    Add Cast method

---
+++ src/flatcollections-array/FlatArray/FlatArray.T/Functional/Functional.Cast.cs
@@ -0,0 +1,29 @@
+﻿namespace System;
+
+partial struct FlatArray<T>
+{
+    // TODO: Add the tests and make public
+    internal FlatArray<TResult> Cast<TResult>()
+    {
+        if (length == default)
+        {
+            return default;
+        }
+
+        if (items is TResult[] resultItems)
+        {
+            return new(length, resultItems);
+        }
+
+        resultItems = new TResult[length];
+
+        var counter = 0;
+        do
+        {
+            resultItems[counter] = (TResult)(object)items![counter]!;
+        }
+        while (++counter < resultItems.Length);
+
+        return new(resultItems, default);
+    }
+}
