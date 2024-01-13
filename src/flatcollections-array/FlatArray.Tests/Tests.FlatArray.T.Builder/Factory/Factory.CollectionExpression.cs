1e48d6957be67be6fe851b0d0a5f6abd8e12f7fc
Andrei Sergeev <a.sergeev.official@gmail.com>
2024-01-13 07:12:52 +0400

    FlatArray.Builder: Add CollectionBuilder tests

---
+++ src/flatcollections-array/FlatArray.Tests/Tests.FlatArray.T.Builder/Factory/Factory.CollectionExpression.cs
@@ -0,0 +1,26 @@
+﻿#if NET8_0_OR_GREATER
+using System;
+using Xunit;
+using static PrimeFuncPack.UnitTest.TestData;
+
+namespace PrimeFuncPack.Core.Tests;
+
+partial class FlatArrayBuilderTest
+{
+    [Fact]
+    public static void ConstructFromCollectionExpression_SourceIsEmpty_ExpectDefault()
+    {
+        FlatArray<string?>.Builder actual = [];
+        actual.VerifyInnerState([], default);
+    }
+
+    [Fact]
+    public static void ConstructFromCollectionExpression_SourceIsNotEmpty_ExpectInnerStateIsSourceCollection()
+    {
+        FlatArray<string?>.Builder actual = [ SomeString, null, EmptyString, WhiteSpaceString, AnotherString ];
+        string?[] expectedItems = [ SomeString, null, EmptyString, WhiteSpaceString, AnotherString ];
+
+        actual.VerifyInnerState(expectedItems, expectedItems.Length);
+    }
+}
+#endif
