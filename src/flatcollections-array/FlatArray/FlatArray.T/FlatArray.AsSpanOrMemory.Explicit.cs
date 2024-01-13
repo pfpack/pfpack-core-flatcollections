1fef1d1bb9c3532c7752fbdf5d76f7536b4e0fdd
Andrei Sergeev <a.sergeev.official@gmail.com>
2024-01-13 14:04:19 +0400

    Refactor: Avoid using InnerToSpan/Memory

--- src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.AsSpanOrMemory.Explicit.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.AsSpanOrMemory.Explicit.cs
@@ -6,11 +6,35 @@ partial struct FlatArray<T>
 {
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public ReadOnlySpan<T> AsSpan()
-        =>
-        InnerAsSpan();
+    {
+        if (length == default)
+        {
+#pragma warning disable IDE0301 // Simplify collection initialization
+            return ReadOnlySpan<T>.Empty;
+#pragma warning restore IDE0301 // Simplify collection initialization
+        }
+
+        if (length == items!.Length)
+        {
+            return new(items);
+        }
+
+        return new(items, 0, length);
+    }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public ReadOnlyMemory<T> AsMemory()
-        =>
-        InnerAsMemory();
+    {
+        if (length == default)
+        {
+            return ReadOnlyMemory<T>.Empty;
+        }
+
+        if (length == items!.Length)
+        {
+            return new(items);
+        }
+
+        return new(items, 0, length);
+    }
 }
--- src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.cs
@@ -31,38 +31,4 @@ public readonly partial struct FlatArray<T> : IEquatable<FlatArray<T>>
     private T[] InnerItems()
         =>
         length == default ? InnerEmptyArray.Value : items!;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private ReadOnlySpan<T> InnerAsSpan()
-    {
-        if (length == default)
-        {
-#pragma warning disable IDE0301 // Simplify collection initialization
-            return ReadOnlySpan<T>.Empty;
-#pragma warning restore IDE0301 // Simplify collection initialization
-        }
-
-        if (length == items!.Length)
-        {
-            return new(items);
-        }
-
-        return new(items, 0, length);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private ReadOnlyMemory<T> InnerAsMemory()
-    {
-        if (length == default)
-        {
-            return ReadOnlyMemory<T>.Empty;
-        }
-
-        if (length == items!.Length)
-        {
-            return new(items);
-        }
-
-        return new(items, 0, length);
-    }
 }
