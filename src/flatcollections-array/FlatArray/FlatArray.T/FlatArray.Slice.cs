f0a9e70793a187b20c095437bdf861213a85803b
Andrei Sergeev <a.sergeev.official@gmail.com>
2024-03-09 18:58:55 +0400

    Add Slice method

---
+++ src/flatcollections-array/FlatArray/FlatArray.T/FlatArray.Slice.cs
@@ -0,0 +1,9 @@
+﻿namespace System;
+
+partial struct FlatArray<T>
+{
+    // TODO: Add the tests and make public
+    internal FlatArray<T> Slice(int start, int length)
+        =>
+        From(this, start, length);
+}
