df7536694e8212d24c6b257ca4dd6062ca5aa9eb
Andrei Sergeev <a.sergeev.official@gmail.com>
2025-01-08 00:46:25 +0400

    Improve suppression of IDE0300/CA1825 for InnerEmptyArray

--- src/flatcollections-array/FlatArray/FlatArray.T/InnerEmptyArray/FlatArray.InnerEmptyArray.cs
+++ src/flatcollections-array/FlatArray/FlatArray.T/InnerEmptyArray/FlatArray.InnerEmptyArray.cs
@@ -1,4 +1,7 @@
-﻿namespace System;
+﻿#pragma warning disable IDE0300 // Simplify collection initialization
+#pragma warning disable CA1825 // Avoid zero-length array allocations
+
+namespace System;
 
 partial struct FlatArray<T>
 {
@@ -7,8 +10,6 @@ partial struct FlatArray<T>
         // Intended for the inner use only, not to pass outside
         // Designed not to expose the inner state of the empty FlatArray and the Builder
 
-#pragma warning disable CA1825 // Avoid zero-length array allocations
         internal static readonly T[] Value = new T[0];
-#pragma warning restore CA1825 // Avoid zero-length array allocations
     }
 }
