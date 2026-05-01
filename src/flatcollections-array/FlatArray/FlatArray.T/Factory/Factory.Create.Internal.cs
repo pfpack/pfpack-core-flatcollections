a52bf6334f6747ac1fbc60a72d49d224ca83d06c
Andrei Sergeev <a.sergeev.official@gmail.com>
2026-05-01 10:29:25 +0400

    release/v1.6.0-rc.1; minor refactor

---
+++ src/flatcollections-array/FlatArray/FlatArray.T/Factory/Factory.Create.Internal.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace System;
+
+partial struct FlatArray<T>
+{
+    // Initializes an instance without creation of a defensive copy
+    // When the input array is empty, the default is returned
+    internal static FlatArray<T> InternalCreate(T[] items)
+    {
+        Debug.Assert(items is not null);
+
+        if (items.Length == default)
+        {
+            return default;
+        }
+
+        return new(items, default);
+    }
+}
